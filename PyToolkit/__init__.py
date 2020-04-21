89e55048a39118f55d71ef367d1f55376a81c2c1
LiangsLi <poales@sina.com>
2020-04-21 16:32:23 +0800

    add __init__

---
+++ PyToolkit/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Created by li huayong on 2020/4/21
+from PyToolkit.easy_ltp import EasyLTP
+from PyToolkit.logger import init_logger, get_logger
+from PyToolkit.timer import Timer
+
+if __name__ == '__main__':
+    pass
