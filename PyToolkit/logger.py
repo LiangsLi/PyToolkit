52c3301e40002be90da2e3eb2c1d1aab4e07b4b7
LiangsLi <poales@sina.com>
2020-04-23 12:09:43 +0800

    update logger

--- PyToolkit/logger.py
+++ PyToolkit/logger.py
@@ -23,22 +23,17 @@ def init_logger(logger_name, log_file=None, is_debug=False, only_console=False):
         logger.setLevel(logging.INFO)
     # handlers:
     c_handler = logging.StreamHandler()
-    f_handler = logging.FileHandler(log_file)
-
-    # level:
     c_handler.setLevel(logging.INFO)
-    f_handler.setLevel(logging.INFO)
-
     c_format = logging.Formatter("%(asctime)s-%(levelname)s-%(message)s")
-    f_format = logging.Formatter("%(asctime)s-%(levelname)s-%(message)s")
     c_handler.setFormatter(c_format)
-    f_handler.setFormatter(f_format)
-
-    # add
     logger.addHandler(c_handler)
     if not only_console:
+        f_handler = logging.FileHandler(log_file)
+        f_handler.setLevel(logging.INFO)
+        f_format = logging.Formatter("%(asctime)s-%(levelname)s-%(message)s")
+        f_handler.setFormatter(f_format)
         logger.addHandler(f_handler)
-    logger.info('===================== NEW LOGGER =========================')
+        logger.info(f'===================== NEW LOGGER:{logger_name} =========================')
     return logger
 
 
@@ -47,4 +42,5 @@ def get_logger(logger_name):
 
 
 if __name__ == '__main__':
-    pass
+    my_logger = init_logger('test', 'test.log')
+    my_logger.info('this is a info')
